799501a443d0efae83a3865f33fce77358aec167
Ruyet <55694828+ruyet@users.noreply.github.com>
2024-12-20 10:03:07 +0100

    Add files via upload

---
+++ Website/assets/js/script.js
@@ -0,0 +1,4 @@
+// Toggle menu once clicked for responsiveness
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('open');
+});
